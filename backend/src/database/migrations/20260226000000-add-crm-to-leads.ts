08bd35a2e590a50ca2f12c470bd5a1db936e2962
William Prado <williamprado@gmail.com>
2026-02-26 03:25:42 -0300

    fix(migration): ignore error if columns already exist in crm_leads

--- backend/src/database/migrations/20260226000000-add-crm-to-leads.ts
+++ backend/src/database/migrations/20260226000000-add-crm-to-leads.ts
@@ -3,31 +3,43 @@ import { QueryInterface, DataTypes } from "sequelize";
 module.exports = {
     up: async (queryInterface: QueryInterface) => {
         // Add assignedUserId to Leads to track agent portfolio
-        await queryInterface.addColumn("crm_leads", "owner_user_id", {
-            type: DataTypes.INTEGER,
-            references: { model: "Users", key: "id" },
-            onUpdate: "CASCADE",
-            onDelete: "SET NULL",
-            allowNull: true
-        });
+        try {
+            await queryInterface.addColumn("crm_leads", "owner_user_id", {
+                type: DataTypes.INTEGER,
+                references: { model: "Users", key: "id" },
+                onUpdate: "CASCADE",
+                onDelete: "SET NULL",
+                allowNull: true
+            });
+        } catch (e) {
+            console.log("Column owner_user_id already exists.");
+        }
 
         // Add pipeline_id to Leads
-        await queryInterface.addColumn("crm_leads", "pipeline_id", {
-            type: DataTypes.INTEGER,
-            references: { model: "Pipelines", key: "id" },
-            onUpdate: "CASCADE",
-            onDelete: "SET NULL",
-            allowNull: true
-        });
+        try {
+            await queryInterface.addColumn("crm_leads", "pipeline_id", {
+                type: DataTypes.INTEGER,
+                references: { model: "Pipelines", key: "id" },
+                onUpdate: "CASCADE",
+                onDelete: "SET NULL",
+                allowNull: true
+            });
+        } catch (e) {
+            console.log("Column pipeline_id already exists.");
+        }
 
         // Add pipeline_stage_id to Leads
-        await queryInterface.addColumn("crm_leads", "stage_id", {
-            type: DataTypes.INTEGER,
-            references: { model: "PipelineStages", key: "id" },
-            onUpdate: "CASCADE",
-            onDelete: "SET NULL",
-            allowNull: true
-        });
+        try {
+            await queryInterface.addColumn("crm_leads", "stage_id", {
+                type: DataTypes.INTEGER,
+                references: { model: "PipelineStages", key: "id" },
+                onUpdate: "CASCADE",
+                onDelete: "SET NULL",
+                allowNull: true
+            });
+        } catch (e) {
+            console.log("Column stage_id already exists.");
+        }
     },
 
     down: async (queryInterface: QueryInterface) => {
