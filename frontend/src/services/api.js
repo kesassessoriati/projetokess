7c727f29966ecfa03c323b192bd80c753bf4995b
williamprado <williamprado10@live.com>
2026-05-25 21:46:57 -0300

    fix: evitar logout indevido ao atualizar pagina

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,17 +6,6 @@ const api = axios.create({
   withCredentials: true,
 });
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.clear();
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  }
-);
-
 export const openApi = axios.create({
   baseURL: getBackendUrl()
 });
